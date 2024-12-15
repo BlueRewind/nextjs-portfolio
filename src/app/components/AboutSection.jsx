031f54fe4f35a569de2050d2ce5744b91056e878
bigbyte-aidan <aidan@bigbyte.digital>
2024-12-15 19:35:39 +0000

    initial

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,9 +10,9 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <section>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8">
               <div>
-                  <h3 className="text-lg font-semibold text-sky-400 mb-3">Frontend</h3>
+                  {/* <h3 className="text-lg font-semibold text-sky-400 mb-3"></h3> */}
                   <ul className="flex flex-wrap gap-4">
                       <SkillsCard title="React" path="/react.svg" alt="React" />
                       <SkillsCard title="TypeScript" path="/images/icon/typescript.png" alt="TypeScript" />
@@ -21,11 +21,6 @@ const TAB_DATA = [
                       <SkillsCard title="Tailwind CSS" path="/images/icon/tailwind.png" alt="Tailwind" />
                       <SkillsCard title="MUI" path="/images/icon/mui.png" alt="MUI" />
                       <SkillsCard title="PixiJS" path="/pixijs.svg" alt="PixiJS" />
-                  </ul>
-              </div>
-              <div>
-                  <h3 className="text-lg font-semibold text-red-400 mb-3">Backend</h3>
-                  <ul className="flex flex-wrap gap-4">
                       <SkillsCard title="Java" path="/java.svg" alt="Java" />
                       <SkillsCard title="Spring Boot" path="/images/icon/spring_boot.png" alt="Spring" />
                       <SkillsCard title="NodeJS" path="/nodejs.svg" alt="NodeJS" />
@@ -33,11 +28,6 @@ const TAB_DATA = [
                       <SkillsCard title="Kafka" path="/kafka.svg" alt="Kafka" />
                       <SkillsCard title="PostgreSQL" path="/images/icon/postgres.png" alt="PostgreSQL" />
                       <SkillsCard title="MongoDB" path="/mongodb.svg" alt="MongoDB" />
-                  </ul>
-              </div>
-              <div>
-                  <h3 className="text-lg font-semibold text-purple-400 mb-3">DevOps</h3>
-                  <ul className="flex flex-wrap gap-4">
                       <SkillsCard title="Docker" path="/docker.svg" alt="Docker" />
                       <SkillsCard title="Kubernetes" path="/images/icon/Kubernetes.png" alt="Kubernetes" />
                       <SkillsCard title="AWS" path="/images/icon/AWS.png" alt="AWS" />
@@ -45,14 +35,6 @@ const TAB_DATA = [
                       <SkillsCard title="Jenkins" path="/images/icon/jenkins.png" alt="Jenkins" />
                       <SkillsCard title="SST" path="/images/icon/SST.png" alt="SST" />
                       <SkillsCard title="DataDog" path="/Datadog.svg" alt="DataDog" />
-                  </ul>
-              </div>
-              <div>
-                  <h3 className="text-lg font-semibold text-green-400 mb-3">Cyber Skills</h3>
-                  <ul className="flex flex-wrap gap-4">
-                      <SkillsCard title="Social Engineering" path="/images/icon/se.png" alt="Social" />
-                      <SkillsCard title="Penetration Testing" path="/images/icon/pentesting.png" alt="PenTest" />
-                      <SkillsCard title="Exploit Development" path="/osed.svg" alt="Exploit" />
                   </ul>
               </div>
           </div>
@@ -95,12 +77,11 @@ const AboutSection = () => {
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" alt="about-image" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+        <h2 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-500 to-pink-500 mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I'm a Software Engineer with over half a decade in professional experience.
-            Designing, developing and maintaining secure, scalable full-stack applications. I 
-            am a part-time bug bounty hunter upskilling in Penetration testing, Malware Development, 
-            and Reverse Engineering.
+          I'm a professional with over 6 years of experience designing, developing, and maintaining secure, 
+          scalable full-stack applications. In my spare time, I do bug bounty hunting and am upskilling in 
+          penetration testing, malware development, and reverse engineering.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
--- src/app/components/SkillsCard.jsx
+++ src/app/components/SkillsCard.jsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 
 export const SkillsCard = ({ title, alt, path }) => {
     return (
-        <li className="flex items-center gap-2">
+        <li className="flex items-center gap-1 bg-[#232323] p-1">
             <Image 
                 src={path} 
                 alt={alt} 
                 width={40}
                 height={40} 
-                className="md:w-10 md:h-10"
+                className="md:w-12 md:h-12"
                 priority={false}
             />
-            <span className="text-sm md:text-base text-white">{title}</span>
         </li>
     );
 };
